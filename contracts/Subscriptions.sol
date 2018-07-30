dab948c1fa0ad7962d092c66fe9da3b7ba785b7f
Kevin Seagraves <k.s.seagraves@gmail.com>
2018-07-30 11:36:57 -0600

    added changeSubscriptionStatus function, added questions, added lastWithdrawal

--- contracts/Subscriptions.sol
+++ contracts/Subscriptions.sol
@@ -8,6 +8,9 @@ contract Subscriptions {
 
 	address owner;
 
+	/* Agent is the address that transfers from the funder to the grantee */
+	address agent;
+
 	mapping(uint => Subscription) public subscriptions;
 
 
@@ -15,37 +18,43 @@ contract Subscriptions {
 			address destination;
 			address recipient;
 			address agent;
+			uint agentRewardPct;
 			uint valuePerPeriod;
-			uint period;
+			uint secondPerTimePeriod;
 			uint expiration;
+			uint lastWithdrawl;
 			uint nextWithdrawl;
 			uint grantId;
+			bool active;
 	}
 
 	event newSubscription(
 		address _destination,
 		address _recipient,
 		address _agent,
+		uint _agentRewardPct,
 		uint _valuePerPeriod,
-		uint _period,
+		uint _secondsPerTimePeriod,
 		uint _expiration,
-		uint _grantId
+		uint _grantId,
+		bool _active
 		);
+	event cancelSubscription(uint _grantId);
+	emit changeSubscriptionStatus(uint _grantId, bool _active);
+
 
 constructor() public {
 
 }
 
-function isValidSubscription() public {
-
-}
-
+/* needs modifier to ensure subscription doesn't or is already expired */
 function createSubscription(
 	address _destination,
 	address _recipient,
 	address _agent,
+	uint _agentRewardPct,
 	uint _valuePerPeriod,
-	uint _period,
+	uint _secondsPerTimePeriod,
 	uint _expiration,
 	uint _grantId
 	)
@@ -55,36 +64,86 @@ function createSubscription(
 		Subscription storage sub = subscriptions[_grantId];
 
 		sub.destination = _destination;
-		sub.recipient =  _recipient;
+		sub.recipient = _recipient;
 		sub.agent = _agent;
+		sub.agentRewardPct = _agentRewardPct;
 		sub.valuePerPeriod = _valuePerPeriod;
-		sub.period = _period;
+		sub.secondsPerTimePeriod = _secondsPerTimePeriod;
 		sub.expiration = _expiration;
 		sub.grantId = _grantId;
-		sub.nextWithdrawl = now + _period;
+		sub.lastWithdrawl = now
+		sub.nextWithdrawl = sub.lastWithdrawl + _secondsPerTimePeriod;
+		sub.active = true;
 
-		emit newSubscription(_destination, _recipient, _agent, _valuePerPeriod, _period, _expiration, _grantId);
+		emit newSubscription(_destination, _recipient, _agent, _agentRewardPct, _valuePerPeriod, _secondsPerTimePeriod, _expiration, _grantId, true);
 }
 
-function editSubscription() public {
 
+
+/* Should be within some sort of bounds, like, must cancel by the 15th to have applied to next month */
+function cancelSubscription(
+	uint _grantId
+	) public {
+		Subscription storage sub = subscriptions[_grantId];
+        sub.expires = now;
+
+		/* need to transfer any funds that have not yet been claimed/transfared via agent */
+
+		emit cancelSubscription(_grantId);
 }
 
-function cancelSubscription() public {
+/* Should be within some sort of bounds, like, must cancel by the 15th to have applied to next month */
+function changeSubscriptionStatus(
+	uint _grantId,
+	bool _active
+	) public {
+		Subscription storage sub = subscriptions[_grantId];
+        sub.active = _active
 
+	/* need to transfer any funds that have not yet been claimed/transfared via agent */
+
+
+		emit changeSubscriptionStatus(_grantId, _active);
 }
 
+/* need to ensure that subscription is active, current blocktime is past nextWithdrawl and check how many period have past since lastWithdrawl */
 function executeSubscription() public {
 
 }
 
+function updateSubscriptionValue() public {
 
-function getSubsrciption() view {
+}
+
+/* is there any difference between a pause function and a cancel function? maybe a cancel function does gabage collecting and uses the gas refund process.  */
+function pauseSubscription() public {}
+
+
+function getSubscription()
+view
+returns
+{
 
 }
 
 function getUserSubscriptions() view {
 
 }
+function isValidSubscription() public {
 
 }
+
+}
+
+
+/*
+Open questions:
+
+Do we want a pause feature on the subscriptions?
+Do we assume that the agent will always be Gitcoin for these subscriptions?
+Seems like having the tip funcitonality on grants page could be good if someone wants to add more value in any given month
+Do we want to have an edit subscription function? Seems like maybe for value?
+What hapens if someone does not claim a payment? how do we ensure they are then able to pull down the full amount, say, three periods later?
+Should a payment accompany createSubsrciption?
+
+ */
